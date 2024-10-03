923258a1addb8edcc4cada610c3f9aff716f09e5
Zenor-git <ratan.pradhan@accenture.com>
2024-10-03 19:46:53 +0530

    done | testing: not done

--- src/store/cart.mjs
+++ src/store/cart.mjs
@@ -29,19 +29,15 @@ export const cartSlice = createSlice({
       state.customPizza.count += 1
       state.customPizza.items.push(action.payload)
       let sum = 0
-      console.log('b sum',sum)
-    //   console.log()
       for (let index = 0; index < state.customPizza.items.length; index++) {
         let element = state.customPizza.items[index];
         sum += element.price
       }
-      console.log('sum',sum)
       state.customPizza.totalPrice = sum
-      console.log(state.customPizza.totalPrice)
     //
     },
     removeCustomPizza: (state,action) => {
-        state.count += 1
+        state.count -= 1
         state.customPizza.count -= 1
         let indexToRemove = state.customPizza.items.indexOf(action.payload)
         let priceToRemove = state.customPizza.items[indexToRemove].price
